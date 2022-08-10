df6eb539de460c9c50d52a6304badaea2500bf29
unknown <user@DESKTOP-VI1L6FA.office.tut.ru>
2022-08-10 17:40:36 +0300

    first edit from work PC

--- homework.py
+++ homework.py
@@ -5,7 +5,7 @@ import time
 
 import requests
 from dotenv import load_dotenv
-from telegram import Bot
+from telegram import Bot, TelegramError
 
 load_dotenv()
 
@@ -38,16 +38,14 @@ def send_message(bot, message):
     chat_id = TELEGRAM_CHAT_ID
     try:
         bot.send_message(chat_id, message)
-        logger.info(f'Bot just sent a message: {message}')
-    except Exception as error:
-        logger.error(f'an error in send_message function: {error}')
+    except TelegramError:
+        raise TelegramError('an error in send_message function')
 
 
 def get_api_answer(current_timestamp):
     """Получаем ответ от API."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-
     try:
         response = requests.get(
             ENDPOINT,
@@ -55,20 +53,13 @@ def get_api_answer(current_timestamp):
             params=params
         )
     except Exception as error:
-        logging.error(f'Ошибка при запросе к API: {error}')
         raise Exception(f'Ошибка при запросе к API: {error}')
-
     if response.status_code != 200:
         status_code = response.status_code
-        logger.error(
-            f'homework_statuses.status_code expected 200, but got '
-            f'{status_code}'
-        )
         raise Exception(
             f'homework_statuses.status_code expected 200, but got '
             f'{status_code}'
         )
-
     response = response.json()
     return response
 
@@ -76,45 +67,35 @@ def get_api_answer(current_timestamp):
 def check_response(response):
     """Проверяем полученный ответ."""
     if type(response) is not dict:
-        logger.error(f'response is not dict, but {type(response)}')
-        raise TypeError(f'response is not dict, but {type(response)}')
-
+        raise TypeError(
+            f'type of response is not a dict, but {type(response)}'
+        )
     try:
         homework_list = response['homeworks']
     except KeyError:
-        logger.error('key "homeworks" is missing')
         raise KeyError('key "homeworks" is missing')
-
+    if type(homework_list) is not list:
+        raise TypeError(
+            f'type of homework_list is not a list, but {type(homework_list)}'
+        )
     try:
         homework = homework_list[0]
     except IndexError:
-        logger.error('homework list is empty')
         raise IndexError('homework list is empty')
-
-    logger.debug(f'homework from check_response is {homework}')
     return homework
 
 
 def parse_status(homework):
     """Проверяем изменение статуса проверки работы."""
     if 'homework_name' not in homework:
-        logger.error('key "homework_name" is missing')
         raise KeyError('key "homework_name" is missing')
     homework_name = homework['homework_name']
-    logger.debug(f'homework_name is "{homework_name}"')
-
     if 'status' not in homework:
-        logger.error('key "status" is missing')
         raise KeyError('key "status" is missing')
     homework_status = homework['status']
-    logger.debug(f'homework_status is "{homework_status}"')
-
-    try:
-        verdict = HOMEWORK_STATUSES[homework_status]
-    except KeyError:
-        logger.error('key {homework_status} not in HOMEWORK_STATUSES')
+    if homework_status not in HOMEWORK_STATUSES:
         raise KeyError('key {homework_status} not in HOMEWORK_STATUSES')
-    logger.debug(f'verdict is "{verdict}"')
+    verdict = HOMEWORK_STATUSES[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
@@ -128,12 +109,6 @@ def check_tokens():
     for key, value in keys.items():
         if value is None:
             logger.critical(f'{key} is missing')
-        logger.debug(f'{key} is {value}')
-
-    logger.debug(
-        f'check_tokens returned '
-        f'{bool(PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID)}'
-    )
     return bool(PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID)
 
 
@@ -141,6 +116,11 @@ def main():
     """Основная логика работы бота."""
     logger.debug('main function is started')
     hw_status = None
+    check_dict = {
+        'homework_name': '',
+        'status': '',
+    }
+    error_list = []
     bot = Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
     logger.debug(f'current_timestamp is {current_timestamp}')
@@ -154,8 +134,10 @@ def main():
         try:
             logger.debug('get_api_answer function is started')
             response = get_api_answer(current_timestamp)
+            logger.debug(f'response is {response}')
             logger.debug('check_response function is started')
             homework = check_response(response)
+            logger.debug(f'homework is {homework}')
             logger.debug('checking for response updates')
             logger.debug(f'number of homeworks: {len(response["homeworks"])}')
 
@@ -179,15 +161,18 @@ def main():
                     logger.debug('send_message function is started')
                     send_message(bot, message)
 
+                    logger.info(f'Bot just sent a message: {message}')
             logger.debug(f'go to sleep for {RETRY_TIME}s')
             time.sleep(RETRY_TIME)
             current_timestamp = int(time.time())
             logger.debug(f'Change current_timestamp to {current_timestamp}')
 
         except Exception as error:
+            error_list.append(error)
             message = f'an error in the program: {error}'
             logger.error(message)
-            send_message(bot, message)
+            if error != error_list[0]:
+                send_message(bot, message)
             time.sleep(RETRY_TIME)
         else:
             pass
